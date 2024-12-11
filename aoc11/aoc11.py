004f840d214121e831742ef7c34763874667159d
dimakor <39400638+dimakor@users.noreply.github.com>
2024-12-11 23:11:13 +0300

    Solution for day 11 part 2

--- aoc11/aoc11.py
+++ aoc11/aoc11.py
@@ -2,6 +2,7 @@ import pathlib
 import sys
 from rich import print
 from tqdm import tqdm
+from functools import cache
 
 def process(puzzle_input):
     """Parse input"""
@@ -33,18 +34,39 @@ def part1(data):
     #print(stones)
     return(len(stones))
             
+@cache
+def total_stones(value, turns_remaining):
+    while turns_remaining > 0:
+        if value == 0:
+            value = 1
+            turns_remaining -= 1
+        elif len(str(value)) % 2 == 0:
+            first_half = str(value)[:len(str(value)) // 2]
+            second_half = str(value)[len(str(value)) // 2:]
+            turns_remaining -= 1
+            total = 0
+            total += total_stones(int(first_half), turns_remaining)
+            total += total_stones(int(second_half), turns_remaining)
+            return total
+        else:
+            value = value * 2024
+            turns_remaining -= 1
+    return 1
 
 def part2(data):
     """Solve part 2"""
-    stones = data
-    for 
+    turns = 75
+    total = 0
+    for n in data:
+        total += total_stones(n, turns)
+    return total
 
 if __name__ == "__main__":
     for path in sys.argv[1:]:
         print(f"{path}:")
         puzzle_input = pathlib.Path(path).read_text().strip()
         data = process(puzzle_input)
-        solution1 = part1(data)
+        #solution1 = part1(data)
         solution2 = part2(data)
-        print("PART1:", solution1)
+        #print("PART1:", solution1)
         print("PART2:", solution2)
