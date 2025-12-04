e1a1b2f6b5c69e61da1bf9b367c50abb607f9874
dimakor <39400638+dimakor@users.noreply.github.com>
2025-12-04 11:44:23 +0300

    Solution for aoc04 2025

--- 2025/aoc04/aoc04.py
+++ 2025/aoc04/aoc04.py
@@ -4,14 +4,58 @@ from rich import print
 
 def process(puzzle_input):
     """Parse input"""
+    m = []
     for line in puzzle_input.split('\n'):
-        
+        m.append(line)
+    return m
+
+def check_condition(m, x, y):
+    """Check if fewer than four rolls of paper in the neighborhood of (x, y) roll"""
+    """ We will use x as row index and y as column index """
+    rows = len(m)
+    cols = len(m[0])
+    count = 0
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < rows and 0 <= ny < cols:
+                if m[nx][ny] == '@':
+                    count += 1
+    return True if count < 4 else False
 
 def part1(data):
     """Solve part 1"""
+    total = 0
+    rows = len(data)
+    cols = len(data[0])
+    for i in range(rows):
+        for j in range(cols):
+            if data[i][j] == '@':
+                if check_condition(data, i, j):
+                    total += 1
+    return total
 
 def part2(data):
     """Solve part 2"""
+    rows = len(data)
+    cols = len(data[0])
+    count = 0
+    while True:
+        removed = 0
+        for i in range(rows):
+            for j in range(cols):
+                if data[i][j] == '@':
+                    if check_condition(data, i, j):
+                        # remove roll of paper
+                        data[i] = data[i][:j] + '.' + data[i][j+1:]
+                        removed += 1
+                        count += 1
+                        #break
+        if removed == 0:
+            break
+    return count
 
 if __name__ == "__main__":
     for path in sys.argv[1:]:
